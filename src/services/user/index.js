70523744a4d175315428b3846630daa77e49a7f8
Engr. Md. Muslim Uddin Arju <mdarju240@gmail.com>
2021-10-11 12:53:52 +0100

    user route

--- src/services/user/index.js
+++ src/services/user/index.js
@@ -11,13 +11,33 @@ router
 router
   .route("/register")
   .post(users.create)
-
+router
+.route("/login")
+.post(users.Login)
   
 router
   .route("/me")
   .get(users.getSingle)
   .put(users.update)
   .delete(users.deleteSingle)
+
+
+
+//routes for google logins
+// authorsRouter.get("/googleLogin", passport.authenticate("google", { scope: ["profile", "email"] }))
+
+// authorsRouter.get("/googleRedirect", passport.authenticate("google"), async (req, res, next) => {
+//   try {
+//     console.log("redirect")
+//     console.log(req.user)
+//     res.cookie("token", req.user.token, {
+//       httpOnly: true,
+//     })
+//     res.redirect(`http://localhost:3000`)
+//   } catch (error) {
+//     next(error)
+//   }
+// })
 
 
 export default router
