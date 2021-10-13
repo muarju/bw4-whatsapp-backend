65597988a8c8896bdd5cc7adc408831eac77d95c
Tomas Berhane <68202596+tomas-collab@users.noreply.github.com>
2021-10-13 13:11:06 +0200

    test clean code

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -1,14 +1,12 @@
-import supertest from "supertest"
-import {server} from "../server/index.js"
 import dotenv from "dotenv"
-import mongoose, { connection } from "mongoose"
+import mongoose from "mongoose"
 import tests from './tests-handlers.js'
-import { it } from "jest-circus"
+
 
 
 
 dotenv.config()
-const request = supertest(server)
+
 
 describe("Testing the testing environment", () => {
     it("should test that true is true", () => {
@@ -17,6 +15,7 @@ describe("Testing the testing environment", () => {
 })
 
 describe("Testing the server", () => {
+
     beforeAll(done => {
         mongoose.connect(process.env.MONGO_TEST_URL)
             .then(() => {
@@ -25,70 +24,23 @@ describe("Testing the server", () => {
             }).catch(err=>console.log(err))
     }) 
 
-    
-  //Test example
-    
-    // it("Description of what iss testing", tests.userCreation)
-
-    
-    
     afterAll(done => {
         mongoose.connection.dropDatabase().then(() => {
             console.log("DB dropped, removed testing DB")
-
+            
             mongoose.connection.close().then(() => {
                 done()
+                })
             })
         })
-    })
-    const validUser = {
-        name:'',
-        email:''
-    }
-    const invalidUser = {
-        name:''
-    }
-
-    const updateUser ={
-        name: "",
-        email: ''
-    }
-
-    const deleteUser = {
-        name: "",
-        email: ''
-    }
-
-    it("should test that the endpoint user/not-existing endpoint is ok",async()=>{
-        const response = await request.get('/user')
-        expect(response.status).toBe(200)
-        expect(response.body.message).toBe('test success')
-    })
-
-    it("should test that /can'tFound endpoint is returning 404",async()=>{
-        const response = await request.get('/not-existing')
-        expect(response.status).toBe(404)
-    })
-
-    it('should test that a POST /user/me is  returning a valid user',async()=>{
-        
-        const response = await request.post('/user/me').send(validUser)
-        expect(response.status).toBe(201)
-        expect(response.body._id).toBeDefined()
-    })
-
-    it('should test that a POST /user/me is returning us an invalid user',async()=>{
-        
-        const response = await request.post('/user/me').send(invalidUser)
-        expect(response.status).toBe(400)
-        expect(response.body.message).toBe('test unsuccessful')
-    })
-
-    it('should test that the GET /user/me endpoint is returning a valid user',async()=>{
-        const response= await request.get('/user')
-        expect(response.status).toBe(200)
-        
-    })
+    
+    it("should test that a POST /user/register is  creating a valid user", tests.userCreation)
+    it("should test that a POST /user/register is  creating an invalid user", tests.invalidUserCreation)
+    it("should test that a POST /user/login is  returning a valid user", tests.validUserLogin)
+    it("should test that a POST /user/login is  returning an invalid user", tests.invalidUserLogin)
+    it("should test that a GET  /user/me is  returning a user", tests.getUserMe)
+    it("should test that a PUT  /user/me is  updating a valid user", tests.updateUserMe)
+    it("should test that a DELETE /user/me is  deleting a user", tests.deleteUserMe) 
 })
 
 
