e8f4ea047dab1b4ab019883f5c375c4cf84ce3fc
Tomas Berhane <68202596+tomas-collab@users.noreply.github.com>
2021-10-13 13:03:16 +0200

    test

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -1,8 +1,11 @@
 import supertest from "supertest"
 import {server} from "../server/index.js"
 import dotenv from "dotenv"
-import mongoose from "mongoose"
+import mongoose, { connection } from "mongoose"
 import tests from './tests-handlers.js'
+import { it } from "jest-circus"
+
+
 
 dotenv.config()
 const request = supertest(server)
@@ -21,10 +24,11 @@ describe("Testing the server", () => {
                 done()
             }).catch(err=>console.log(err))
     }) 
+
     
   //Test example
     
-    it("Description of what iss testing", tests.userCreation)
+    // it("Description of what iss testing", tests.userCreation)
 
     
     
@@ -36,6 +40,54 @@ describe("Testing the server", () => {
                 done()
             })
         })
+    })
+    const validUser = {
+        name:'',
+        email:''
+    }
+    const invalidUser = {
+        name:''
+    }
+
+    const updateUser ={
+        name: "",
+        email: ''
+    }
+
+    const deleteUser = {
+        name: "",
+        email: ''
+    }
+
+    it("should test that the endpoint user/not-existing endpoint is ok",async()=>{
+        const response = await request.get('/user')
+        expect(response.status).toBe(200)
+        expect(response.body.message).toBe('test success')
+    })
+
+    it("should test that /can'tFound endpoint is returning 404",async()=>{
+        const response = await request.get('/not-existing')
+        expect(response.status).toBe(404)
+    })
+
+    it('should test that a POST /user/me is  returning a valid user',async()=>{
+        
+        const response = await request.post('/user/me').send(validUser)
+        expect(response.status).toBe(201)
+        expect(response.body._id).toBeDefined()
+    })
+
+    it('should test that a POST /user/me is returning us an invalid user',async()=>{
+        
+        const response = await request.post('/user/me').send(invalidUser)
+        expect(response.status).toBe(400)
+        expect(response.body.message).toBe('test unsuccessful')
+    })
+
+    it('should test that the GET /user/me endpoint is returning a valid user',async()=>{
+        const response= await request.get('/user')
+        expect(response.status).toBe(200)
+        
     })
 })
 
--- src/__tests__/tests-handlers.js
+++ src/__tests__/tests-handlers.js
@@ -1,19 +1,71 @@
-const userCreation= async () => {
-    const validUser={
+const validMe = {
+        name: 'JestUser',
+        surname: "JestSurname",
+        email: "JestEmail@test.com"
+      }
+
+const validUser={
         name: 'JestUser',
         surname: "JestSurname",
         email: "JestEmail@test.com",
         password: '123456'
-    }
+}
+const invalidUser = {
+        name:'jestUser',
+}
+
+const validLogin = {
+        name:'jestUser',
+        email:'jest@gmail.com'
+}
+
+const invalidLogin = {
+        name:'jestUser',
+}
+
+const validUserCreation= async() => {
     const response = await request.post('/user/register').send(validUser)
-    
     expect(response.status).toBe(201)
     expect(response.body._id).toBeDefined()
-    
+}
+
+const invalidUserCreation = async() =>{
+    const response = await request.post('/user/register').send(invalidUser)
+    expect(response.status).toBe(401) 
+}
+
+const validUserLogin = async() =>{ 
+    const response = await request.post('/user/login').send(validLogin)
+    expect(response.status).toBe(200)
+}
+
+const invalidUserLogin = async() =>{
+    const response = await request.post('/user/login').send(invalidLogin)
+    expect(response.status).toBe(404)
+}
+
+const getUserMe = async() =>{
+    const response = await request.get('/user/me')
+    expect(response.status).toBe(200)
+} 
+const updateUserMe = async() =>{
+    const response = await request.put('/user/me')
+    expect(response.body).toEqual(validMe)
+    expect(typeof response.body.name,response.body.email,response.body.password).toBe('string')
+}
+const deleteUserMe = async() =>{
+    const response = await request.delete('/user/me')
+    expect(response.status).toBe(204)
 }
 
 const tests = {
-    userCreation:userCreation
+    userCreation:validUserCreation,
+    invalidUserCreation:invalidUserCreation,
+    validUserLogin:validUserLogin,
+    invalidUserLogin:invalidUserLogin,
+    getUserMe:getUserMe,
+    updateUserMe:updateUserMe,
+    deleteUserMe:deleteUserMe
 }
 
 export default tests
