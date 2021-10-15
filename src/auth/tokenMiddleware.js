943e7e2b8ad61298005c37d355078b182f847e7c
Koulinn <drdverzola@gmail.com>
2021-10-15 13:06:35 +0100

    removed comments

--- src/auth/tokenMiddleware.js
+++ src/auth/tokenMiddleware.js
@@ -19,20 +19,7 @@ export const tokenMiddleware = async(req,res,next)=>{
             }
         }
 
-        // if(!req.headers.authorization){
-        //     next(createHttpError(401,'please provide credentials'))
-        // }else{
-        //     const token = req.headers.authorization.split(" ")[1]
-        //     const decodedToken = await verifyJWTToken(token)
-        //     console.log(decodedToken._id)
-        //     const user = await UserModel.findById(decodedToken._id)
-        //     if(user){
-        //         req.user = user
-        //         next()
-        //     }else{
-        //         next(createHttpError(404,'user not found'))
-        //     }
-        // } 
+        
     } catch (error) {
         next(createHttpError(401,"Token not valid"))
     }
